a0677acb6fe5298373ff4f4565c10ccb7960d037
sharck77 <67459068+sharck77@users.noreply.github.com>
2025-04-08 14:39:09 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
-  base: '/BusinessResilienceApp/', // <-- repo name
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
-});
+  optimizeDeps: {
+    include: [
+      '@radix-ui/react-tabs',
+      'framer-motion',
+      '@radix-ui/react-presence',
+      '@radix-ui/react-roving-focus',
+      '@radix-ui/react-collection',
+    ],
+  },
+})
